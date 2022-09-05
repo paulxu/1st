3de9a64227a59a07d73fb3222f7fece9012cde25
Paul Xu <paulxu2005@gmail.com>
2022-09-05 16:38:58 -0400

    update

--- cleanup-decrypted.sh
+++ cleanup-decrypted.sh
@@ -5,11 +5,16 @@ rm -f windows-terminal-user-ssh*tgz
 
 rm -f cp-ssh.sh.enc
 rm -f cp-ssh.sh
+
 rm -f openVPNClientConfig.ovpn.enc
 rm -f openVPNClientConfig.ovpn
 
 rm -f _2.enc
 rm -f _2
+
+rm -f bitbucket-export
+rm -f bitbucket-export.enc
+
 rm -rf home
 rm -rf ssh
 rm -rf bash_env
